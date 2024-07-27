c8eb1b00a9d07e112c1363ee6ed25d4ff52e80de
janko <janko@JK78.home>
2024-07-27 10:52:12 +0200

    - update classes

---
+++ core/src/main/java/org/minuteflow/core/api/exception/EntityNotFoundException.java
@@ -0,0 +1,43 @@
+package org.minuteflow.core.api.exception;
+
+/*-
+ * ========================LICENSE_START=================================
+ * minuteflow-core
+ * %%
+ * Copyright (C) 2024 Jan Komrska
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+
+public class EntityNotFoundException extends BaseException {
+    private static final long serialVersionUID = 8365062468933536645L;
+
+    //
+
+    public EntityNotFoundException() {
+        super();
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
